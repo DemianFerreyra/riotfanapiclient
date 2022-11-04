a815079cc737d842997a13c5c1321fd1aa202e42
DemianFerreyra <demiancra@gmail.com>
2022-11-03 22:49:15 -0300

    detail in progress...

--- src/container/detailpage.tsx
+++ src/container/detailpage.tsx
@@ -19,7 +19,26 @@ const DetailPage = () => {
 
   return (
     <article className={s.DetailPage}>
-
+      <article className={s.imagesZone}>
+        <section className={s.characterImg}>
+          <img src={Character[0].pngURL} alt={Character[0].name} />
+          <h1>AGENTE</h1>
+          <h2>{Character[0].name}</h2>
+        </section>
+        <section className={s.carousel}>
+          {
+            Character[0].images.map((img: any, index: any) => (
+              <img src={img} alt={`${Character[0].name}_${index}`} />
+            ))
+          }
+        </section>
+      </article>
+      <article className={s.characterInfo}>
+       <p>{Character[0].description}</p>
+       <section>
+        <img src={`https://vemaps.com/uploads/img/${Character[0].country}-07.png`} alt={`${Character[0].country}_Map`} />
+       </section>
+      </article>
     </article>
   );
 };
